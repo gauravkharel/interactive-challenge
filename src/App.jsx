50c4d1a9eed0312f6adaf353e2c3dcba766d58cd
Gaurav Kharel <kharel619@gmail.com>
2025-04-29 16:23:06 +0545

    Moved images to public

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import CommentSection from "./CommentSection";
-import CommentForm from "./CommentForm";
 const App = () => {
   return (
     <div className="mt-7 flex flex-col items-center min-h-screen bg-gray-100">    
